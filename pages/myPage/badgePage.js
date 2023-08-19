c45958d1db00d605faf3fc0ef10ecaf413356589
jungmiinn <76022058+jungmiinn@users.noreply.github.com>
2023-08-19 19:53:17 +0900

    Feat:Issue3 완료(게시물 컴포넌트)

---
+++ pages/myPage/badgePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import badge from "./badgePage/badge"
+
+function badgePage() {
+    return(
+        <badge>hi</badge>
+    )
+  
+}
+
+export default badgePage; 
---
+++ pages/myPage/badgePage/badge.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function badge() {
+  const style = {
+    badgeimg:{
+        width: '120px',
+        height: '120px',
+        backgroundColor: 'pink'
+    }
+  }
+
+
+
+  return <div>
+    <div style={style.badgeimg}>뱃지 이미지</div>
+    <div>뱃지 이름</div>
+    <div>달성 날짜</div>
+  </div>
+}
+
