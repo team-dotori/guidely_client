8a14fee102c505fcb7daea898df878f802146a7f
HyeonJunSong <music9801@naver.com>
2023-08-22 11:42:54 +0900

    Chore: 설정파일
    next configuration 파일 수정
    글로벌 스타일 및 기타 전역 설정을 위한 _app.js 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      // {
+      //   source: `http://${process.env.GUIDELY_SERVER_BASE_URL}/path:`,
+      //   destination: "/path:",
+      // },
+      {
+        source: "/entrancePage",
+        destination: "/entrancePage/entrancePage",
+      },
+      {
+        source: "/api/signIn/kakao",
+        destination: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=http://${process.env.GUIDELY_CLIENT_BASE_URL}/entrancePage`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
