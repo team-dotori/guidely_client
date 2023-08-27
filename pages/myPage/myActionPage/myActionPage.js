355e51b60800ef38d61faf766d3ae0709f2cd2c0
HyeonJunSong <music9801@naver.com>
2023-08-27 14:12:32 +0900

    Feat: 내 활동 페이지 UI
    카카오 지도 API 불러옴
    확대 축소 오류, 모바일에서 내 위치 받아오기 오류 있음

---
+++ pages/myPage/myActionPage/myActionPage.js
@@ -0,0 +1,11 @@
+import AppBar from "./appBar";
+import MapTab from "./tabMap";
+
+export default function MyActionPage() {
+  return (
+    <div>
+      <AppBar />
+      <MapTab />
+    </div>
+  );
+}
