278e33c84944be6390e9393ecddf348fa0fc8dae
HyeonJunSong <music9801@naver.com>
2023-09-03 18:03:18 +0900

    Feat: 내 활동 로그

--- pages/npwd/myPage/myActionPage/myActionPage.js
+++ pages/npwd/myPage/myActionPage/myActionPage.js
@@ -1,11 +1,35 @@
 import AppBar from "@/components/npwd/myPage/myActionPage/appBar";
+import LogTab from "@/components/npwd/myPage/myActionPage/tabLog";
 import MapTab from "@/components/npwd/myPage/myActionPage/tabMap";
+import { useEffect, useState } from "react";
 
 export default function MyActionPage() {
+  const [reportList, setReportList] = useState([]);
+  const [curTab, setCurTab] = useState(true); // true: map, false: log
+
+  useEffect(() => {
+    fetch("/api/guidely/api/users/declarations/1").then((res) => {
+      res.json().then((res) => {
+        setReportList(res);
+        // category : "BRAILLE"
+        // contents : "This is test"
+        // imgUrl : null
+        // likeCount : 0
+        // location : {id: 1, latitude: 37.123, longitude: -122.672, address: '대학로21길', buildingName: '경북대학교', …}
+        // risk : "HIGH"
+        // specification : "점자 오류"
+      });
+    });
+  }, []);
+
   return (
     <div>
-      <AppBar />
-      <MapTab />
+      <AppBar curTab={curTab} setCurTab={setCurTab} />
+      {curTab ? (
+        <MapTab reportList={reportList} />
+      ) : (
+        <LogTab reportList={reportList} />
+      )}
     </div>
   );
 }
