523b04f0779440199776985fe1ce8deaba5992c4
HyeonJunSong <51317791+HyeonJunSong@users.noreply.github.com>
2023-09-07 19:48:50 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export function middleware(req: NextRequest) {
   ) {
     return NextResponse.next({
       headers: {
-        accessToken: req.cookies.get("accessToken").value,
+        "accessToken": req.cookies.get("accessToken").value,
       },
     });
   }
